01402a6dd80d4a0af467e7faee88eaffc6ffe070
Julien Capellari <julien.capellari@gmail.com>
2024-06-28 19:24:36 +0200

    chore: setup mui

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+'use client';
+
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = extendTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+export default theme;
