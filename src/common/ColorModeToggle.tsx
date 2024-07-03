976e902786bd346b8359377e7a62e752d2566269
Julien Capellari <julien.capellari@gmail.com>
2024-07-03 23:48:08 +0200

    fix: ColorModeToggle while loading page

--- src/common/ColorModeToggle.tsx
+++ src/common/ColorModeToggle.tsx
@@ -5,6 +5,7 @@ import Brightness7Icon from '@mui/icons-material/Brightness7';
 import IconButton from '@mui/material/IconButton';
 import NoSsr from '@mui/material/NoSsr';
 import Paper from '@mui/material/Paper';
+import Skeleton from '@mui/material/Skeleton';
 import { useColorScheme, type SxProps } from '@mui/material/styles';
 import { useCallback } from 'react';
 
@@ -24,15 +25,15 @@ export default function ColorModeToggle({ sx }: ColorModeToggleProps) {
   // Render
   return (
     <Paper sx={[{ borderRadius: 9999 }, ...(Array.isArray(sx) ? sx : [sx])]}>
-      <IconButton
-        color="inherit" aria-label="Switch theme"
-        onClick={handleClick}
-        sx={{ m: 0.5 }}
-      >
-        <NoSsr>
-          { actualMode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon /> }
-        </NoSsr>
-      </IconButton>
+      <NoSsr fallback={<Skeleton variant="circular" width={40} height={40} sx={{ m: 0.5 }} />}>
+        <IconButton
+          color="inherit" aria-label="Switch theme"
+          onClick={handleClick}
+          sx={{ m: 0.5 }}
+        >
+            { actualMode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon /> }
+        </IconButton>
+      </NoSsr>
     </Paper>
   );
 }
