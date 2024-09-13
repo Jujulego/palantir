d6bd1e080822eb6aca1488f1579cd3f88f0c51dd
Julien Capellari <julien.capellari@gmail.com>
2024-09-13 17:43:17 +0200

    feat: remove feature flags (#76)

--- app/locate/[ip]/page.tsx
+++ app/locate/[ip]/page.tsx
@@ -1,17 +1,8 @@
 import Box from '@mui/material/Box';
-import { FlagValues } from '@vercel/flags/react';
 import ipaddr from 'ipaddr.js';
 import { notFound } from 'next/navigation';
 
 import BigDataCloudCard from '@/src/big-data-cloud/BigDataCloudCard';
-import {
-  precomputeFlags,
-  showBigDataCloud,
-  showIpData,
-  showIpGeolocation,
-  showIpInfo,
-  showIpQuality
-} from '@/src/flags';
 import IpDataCard from '@/src/ip-data/IpDataCard';
 import IpGeolocationCard from '@/src/ip-geolocation/IpGeolocationCard';
 import IpInfoCard from '@/src/ip-info/IpInfoCard';
@@ -21,26 +12,15 @@ import IpQualityCard from '@/src/ip-quality/IpQualityCard';
 export interface LocateIpPageProps {
   readonly params: {
     readonly ip: string;
-  },
-  readonly searchParams: {
-    readonly code: string;
   }
 }
 
-export default async function LocateIpPage({ params, searchParams }: LocateIpPageProps) {
+export default async function LocateIpPage({ params }: LocateIpPageProps) {
   const ip = decodeURIComponent(params.ip);
 
   if (!ipaddr.isValid(ip)) {
     notFound();
   }
-
-  const flags = {
-    showBigDataCloud: await showBigDataCloud(searchParams.code, precomputeFlags),
-    showIpData: await showIpData(searchParams.code, precomputeFlags),
-    showIpGeolocation: await showIpGeolocation(searchParams.code, precomputeFlags),
-    showIpInfo: await showIpInfo(searchParams.code, precomputeFlags),
-    showIpQuality: await showIpQuality(searchParams.code, precomputeFlags),
-  };
 
   return (
     <>
@@ -56,14 +36,12 @@ export default async function LocateIpPage({ params, searchParams }: LocateIpPag
           overflow: 'auto',
         }}
       >
-        { flags.showBigDataCloud && <BigDataCloudCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} /> }
-        { flags.showIpData && <IpDataCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} /> }
-        { flags.showIpGeolocation && <IpGeolocationCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} /> }
-        { flags.showIpInfo && <IpInfoCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} /> }
-        { flags.showIpQuality && <IpQualityCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} /> }
+        <BigDataCloudCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} />
+        <IpDataCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} />
+        <IpGeolocationCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} />
+        <IpInfoCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} />
+        <IpQualityCard ip={ip} sx={{ flexShrink: 0, pointerEvents: 'auto' }} />
       </Box>
-
-      <FlagValues values={flags} />
     </>
   );
 }
