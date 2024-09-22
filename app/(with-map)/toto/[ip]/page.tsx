03311b3f02941297e664292bce80eb5a9e358dbc
Julien Capellari <julien.capellari@gmail.com>
2024-09-22 23:23:26 +0200

    feat: load ip info data

---
+++ app/(with-map)/toto/[ip]/page.tsx
@@ -0,0 +1,20 @@
+import MapboxMarker from '@/components/mapbox/MapboxMarker';
+import { fetchIpInfo } from '@/data/ip-info';
+
+// Page
+export interface TotoIpPageProps {
+  readonly params: {
+    readonly ip: string;
+  }
+}
+
+export default async function TotoIpPage({ params }: TotoIpPageProps) {
+  const { location } = await fetchIpInfo(params.ip);
+
+  return location?.coordinates && (
+    <MapboxMarker
+      lat={location.coordinates.latitude}
+      lng={location.coordinates.longitude}
+    />
+  );
+}
--- app/(with-map)/toto/page.tsx
+++
@@ -1,5 +0,0 @@
-import MapboxMarker from '@/components/mapbox/MapboxMarker';
-
-export default function TotoPage() {
-  return <MapboxMarker lat={49} lng={2} />
-}
