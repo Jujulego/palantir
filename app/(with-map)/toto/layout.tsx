cb8a7e1e64c865ec8187dd0d774397a27fbb847d
Julien Capellari <julien.capellari@gmail.com>
2024-09-24 00:55:58 +0200

    feat: add ip search bar

--- app/(with-map)/toto/layout.tsx
+++ app/(with-map)/toto/layout.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import ColorModeToggle from '@/components/common/ColorModeToggle';
+import IpSearchBar from '@/components/common/IpSearchBar';
 import Paper from '@mui/material/Paper';
 import Toolbar from '@mui/material/Toolbar';
-import type { ReactNode } from 'react';
+import { useRouter, useSelectedLayoutSegments } from 'next/navigation';
+import { type ReactNode, useCallback, useMemo } from 'react';
 
 // Component
 export interface TotoLayoutProps {
@@ -9,6 +13,15 @@ export interface TotoLayoutProps {
 }
 
 export default function TotoLayout({ children }: TotoLayoutProps) {
+  const segments = useSelectedLayoutSegments();
+  const router = useRouter();
+
+  const ip = useMemo(() => segments[0] && decodeURIComponent(segments[0]), [segments[0]]);
+  const handleSearch = useCallback((ip: string) => {
+    router.push(`/toto/${encodeURIComponent(ip)}`);
+  }, [router]);
+
+  // Render
   return <>
     <Toolbar
       component="header"
@@ -22,6 +35,10 @@ export default function TotoLayout({ children }: TotoLayoutProps) {
         }
       }}
     >
+      <Paper sx={{ p: 0.5, borderRadius: 9999 }}>
+        <IpSearchBar ip={ip} onSearch={handleSearch} />
+      </Paper>
+
       <Paper sx={{ ml: 'auto', p: 0.5, borderRadius: 9999 }}>
         <ColorModeToggle />
       </Paper>
---
+++ components/common/IpSearchBar.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import SearchIcon from '@mui/icons-material/Search';
+import CircularProgress from '@mui/material/CircularProgress';
+import IconButton from '@mui/material/IconButton';
+import { styled, type SxProps } from '@mui/material/styles';
+import ipaddr from 'ipaddr.js';
+import { type ChangeEvent, type FormEvent, useCallback, useEffect, useMemo, useState, useTransition } from 'react';
+
+// Component
+export interface IpSearchBarProps {
+  readonly ip?: string;
+  readonly sx?: SxProps;
+  readonly onSearch: (ip: string) => void;
+}
+
+export default function IpSearchBar({ ip, sx, onSearch }: IpSearchBarProps) {
+  const [isSearching, startSearch] = useTransition();
+  const [search, setSearch] = useState(ip ?? '');
+  const isValid = useMemo(() => ipaddr.isValid(search), [search]);
+
+  useEffect(() => {
+    setSearch(ip ?? '');
+  }, [ip]);
+
+  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  }, []);
+
+  const handleSearch = useCallback((ip: string) => {
+    startSearch(() => onSearch(ip));
+  }, [onSearch]);
+
+  const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleSearch(search);
+  }, [search, handleSearch]);
+
+  // Render
+  return (
+    <SearchForm sx={sx} onSubmit={handleSubmit}>
+      <SearchInput
+        type="search" placeholder="Adresse IP" required
+        value={search} onChange={handleChange}
+      />
+
+      { isSearching ? (
+        <CircularProgress size={24} sx={{ m: 1, flex: '0 0 auto' }} />
+      ) : (
+        <IconButton
+          color="inherit" type="submit" disabled={!isValid}
+          aria-label="Search"
+          sx={{ flex: '0 0 auto' }}
+        >
+          <SearchIcon />
+        </IconButton>
+      ) }
+    </SearchForm>
+  );
+}
+
+// Utils
+const SearchForm = styled('form')(({ theme }) => ({
+  display: 'flex',
+  gap: theme.spacing(1),
+}));
+
+const SearchInput = styled('input')(({ theme }) => ({
+  height: 40,
+  width: 246,
+  flex: '0 0 auto',
+  paddingLeft: theme.spacing(2.5),
+  backgroundColor: 'transparent',
+  border: 'none',
+  outline: 'none',
+}));
