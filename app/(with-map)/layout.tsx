c7f8c3dd5c6b2bdbfa7c10c3474e5a1a86ff1781
Julien Capellari <julien.capellari@gmail.com>
2024-09-26 00:45:23 +0200

    feat: add toolbar

--- app/(with-map)/layout.tsx
+++ app/(with-map)/layout.tsx
@@ -1,14 +1,35 @@
+import ColorModeToggle from '@/components/common/ColorModeToggle';
 import MapboxMap from '@/components/mapbox/MapboxMap';
+import { Toolbar } from '@mui/material';
+import Paper from '@mui/material/Paper';
 import type { ReactNode } from 'react';
 
+// Layout
 export interface WithMapLayoutProps {
   readonly children: ReactNode;
 }
 
 export default function WithMapLayout({ children }: WithMapLayoutProps) {
-  return (
+  return <>
+    <Toolbar
+      component="header"
+      sx={{
+        flexShrink: 0,
+        zIndex: 'appBar',
+        pointerEvents: 'none',
+
+        '& > *': {
+          pointerEvents: 'auto',
+        }
+      }}
+    >
+      <Paper sx={{ ml: 'auto', p: 0.5, borderRadius: 9999 }}>
+        <ColorModeToggle />
+      </Paper>
+    </Toolbar>
+
     <MapboxMap>
       { children }
     </MapboxMap>
-  );
+  </>;
 }
--- app/(with-map)/page.tsx
+++ app/(with-map)/page.tsx
@@ -1,3 +1,4 @@
+// Page
 export default function RootPage() {
   return null;
 }
