36f5ff7d8ebf3919744f098cc99f2837917ab070
Julien Capellari <julien.capellari@gmail.com>
2024-09-27 00:46:25 +0200

    feat: add mapbox marker

--- app/(with-map)/page.tsx
+++ app/(with-map)/page.tsx
@@ -1,8 +1,10 @@
+import MapboxMarker from '@/components/mapbox/MapboxMarker';
 import MapboxSpin from '@/components/mapbox/MapboxSpin';
 
 // Page
 export default function RootPage() {
-  return (
+  return (<>
     <MapboxSpin />
-  );
+    <MapboxMarker latitude={49} longitude={2} />
+  </>);
 }
