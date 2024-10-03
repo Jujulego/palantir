c551913d53660c6b74508ab0c7e91df1b8f14aa2
Julien Capellari <julien.capellari@gmail.com>
2024-10-03 17:17:37 +0200

    feat: add asn data

--- app/(with-map)/ip/[ip]/page.tsx
+++ app/(with-map)/ip/[ip]/page.tsx
@@ -5,11 +5,12 @@ import MapboxFlyTo from '@/components/mapbox/MapboxFlyTo';
 import MapboxMarker from '@/components/mapbox/MapboxMarker';
 import MapboxSpin from '@/components/mapbox/MapboxSpin';
 import { fetchIpInfo } from '@/data/sources/ip-info';
+import HubIcon from '@mui/icons-material/Hub';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import LocationCityIcon from '@mui/icons-material/LocationCity';
 import ipaddr from 'ipaddr.js';
 
 // Page
@@ -23,7 +24,7 @@ export default async function WithMapIpPage({ params }: WithMapIpPageProps) {
   const ip = decodeURIComponent(params.ip);
   const parsed = ipaddr.parse(ip);
 
-  const { hostname, address, coordinates } = await fetchIpInfo(ip);
+  const { hostname, address, asn, coordinates } = await fetchIpInfo(ip);
 
   // Render
   return <>
@@ -50,15 +51,24 @@ export default async function WithMapIpPage({ params }: WithMapIpPageProps) {
         ) }
       </Box>
 
-      <Divider />
+      <Divider sx={{ mb: 1 }} />
 
       { address && (
-        <Box sx={{ display: 'flex', alignItems: 'center', px: 2.5, py: 2, gap: 2 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', px: 2.5, py: 1, gap: 2 }}>
           <LocationCityIcon color="primary" />
 
           <Typography>
             <AddressTypography address={address} />
           </Typography>
+        </Box>
+      ) }
+
+      { asn && (
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'auto 1fr', alignItems: 'center', px: 2.5, py: 1, columnGap: 2 }}>
+          <HubIcon color="primary" sx={{ gridRow: 'span 2' }} />
+
+          <Typography>{ asn.asn }</Typography>
+          <Typography noWrap>{ asn.name }</Typography>
         </Box>
       ) }
     </Paper>
