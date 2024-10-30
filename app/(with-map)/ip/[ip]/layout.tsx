d3d52163f79a469f70824d556989d416458c566b
Julien Capellari <julien.capellari@gmail.com>
2024-10-31 00:10:32 +0100

    feat: simplify and move image to layout

--- app/(with-map)/ip/[ip]/layout.tsx
+++ app/(with-map)/ip/[ip]/layout.tsx
@@ -1,3 +1,6 @@
+import computerPng from '@/assets/computer.png';
+import ColoredImage from '@/components/common/ColoredImage';
+import Box from '@mui/material/Box';
 import ipaddr from 'ipaddr.js';
 import { redirect } from 'next/navigation';
 import type { ReactNode } from 'react';
@@ -16,5 +19,22 @@ export default function WithMapIpLayout({ children, params }: WithMapIpLayoutPro
     redirect('/');
   }
 
-  return children;
+  return (<>
+    <Box
+      sx={{
+        position: 'relative',
+        height: 230,
+        flexShrink: 0,
+        color: 'action.selected',
+      }}
+    >
+      <ColoredImage
+        src={computerPng}
+        alt="computer"
+        style={{ position: 'absolute', bottom: 0, left: 'calc(50% - 75px)', height: 'auto', width: 150 }}
+      />
+    </Box>
+
+    { children }
+  </>);
 }
--- app/(with-map)/ip/[ip]/page.tsx
+++ app/(with-map)/ip/[ip]/page.tsx
@@ -1,11 +1,8 @@
-import botPng from '@/assets/bot.png';
 import computerPng from '@/assets/computer.png';
-import datacenterPng from '@/assets/datacenter.png';
 import { MergedAsnMenu } from '@/components/asn/MergedAsnMenu';
 import ColoredImage from '@/components/common/ColoredImage';
 import { MergedLocationMenu } from '@/components/location/MergedLocationMenu';
 import { mergeIpMetadata } from '@/data/ip-metadata';
-import { validateSourceIdParam } from '@/data/sources';
 import { fetchBigDataCloud } from '@/data/sources/big-data-cloud';
 import { fetchIpData } from '@/data/sources/ip-data';
 import { fetchIpGeolocation } from '@/data/sources/ip-geolocation';
@@ -39,73 +36,39 @@ export default async function WithMapIpPage({ params }: WithMapIpPageProps) {
     fetchBigDataCloud(ip),
   ]));
 
-  const is_bot = tags.some((tag) => tag.label === 'bot');
-  const is_datacenter = tags.some((tag) => tag.label === 'datacenter');
-
   // Render
-  return <>
-    <Box
-      sx={{
-        position: 'relative',
-        height: 230,
-        flexShrink: 0,
-        color: 'action.selected',
-      }}
-    >
-      { is_bot ? (
-        <ColoredImage
-          src={botPng}
-          alt="bot"
-          style={{ position: 'absolute', bottom: 0, left: 'calc(50% - 75px)', height: 'auto', width: 150 }}
-        />
-      ) : is_datacenter ? (
-        <ColoredImage
-          src={datacenterPng}
-          alt="datacenter"
-          style={{ position: 'absolute', bottom: 0, left: 'calc(50% - 75px)', height: 'auto', width: 150 }}
-        />
-      ) : (
-        <ColoredImage
-          src={computerPng}
-          alt="computer"
-          style={{ position: 'absolute', bottom: 0, left: 'calc(50% - 75px)', height: 'auto', width: 150 }}
-        />
+  return <Paper component="main" square sx={{ flex: '1 0 auto', pb: 4 }}>
+    <Box sx={{ px: 2.5, py: 2 }}>
+      <Typography component="h1" variant="h5">{ parsed.toString() }</Typography>
+      { hostname && (
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>{ hostname }</Typography>
       ) }
     </Box>
 
-    <Paper component="main" square sx={{ flex: '1 0 auto', pb: 4 }}>
-      <Box sx={{ px: 2.5, py: 2 }}>
-        <Typography component="h1" variant="h5">{ parsed.toString() }</Typography>
-        { hostname && (
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>{ hostname }</Typography>
-        ) }
-      </Box>
+    <Divider />
 
-      <Divider />
+    <List>
+      { location.length && (
+        <MergedLocationMenu options={location} />
+      ) }
 
-      <List>
-        { location.length && (
-          <MergedLocationMenu options={location} />
-        ) }
+      { asn.length && (
+        <MergedAsnMenu options={asn} />
+      ) }
 
-        { asn.length && (
-          <MergedAsnMenu options={asn} />
-        ) }
+      { tags.length > 0 && (
+        <ListItem disablePadding sx={{ minHeight: 56, px: 2 }}>
+          <ListItemIcon sx={{ minWidth: 40 }}>
+            <LabelIcon color="primary" />
+          </ListItemIcon>
 
-        { tags.length > 0 && (
-          <ListItem disablePadding sx={{ minHeight: 56, px: 2 }}>
-            <ListItemIcon sx={{ minWidth: 40 }}>
-              <LabelIcon color="primary" />
-            </ListItemIcon>
-
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, flexWrap: 'wrap' }}>
-              { tags.map((tag) => (
-                <Chip key={tag.label} label={tag.label} size="small" color={tag.color} />
-              )) }
-            </Box>
-          </ListItem>
-        ) }
-      </List>
-    </Paper>
-  </>;
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, flexWrap: 'wrap' }}>
+            { tags.map((tag) => (
+              <Chip key={tag.label} label={tag.label} size="small" color={tag.color} />
+            )) }
+          </Box>
+        </ListItem>
+      ) }
+    </List>
+  </Paper>;
 }
