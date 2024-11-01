5090c540b04e18e145d0d525011d9f9f3a51bcf6
Julien Capellari <julien.capellari@gmail.com>
2024-11-01 15:46:53 +0100

    feat: add syntax coloration

--- components/PayloadDialog.tsx
+++ components/PayloadDialog.tsx
@@ -1,6 +1,15 @@
+'use client';
+
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import { styled } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import json from 'react-syntax-highlighter/dist/esm/languages/prism/json';
+import 'prismjs/themes/prism-tomorrow.min.css';
+
+SyntaxHighlighter.registerLanguage('json', json);
 
 // Component
 export interface PayloadDialogProps {
@@ -10,10 +19,27 @@ export interface PayloadDialogProps {
 }
 
 export default function PayloadDialog({ payload, open, onClose }: PayloadDialogProps) {
+  const formatted = useMemo(() => JSON.stringify(payload, null, 2), [payload]);
+  
   return <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
     <DialogTitle>Raw Payload</DialogTitle>
-    <DialogContent>
-      <pre><code>{ JSON.stringify(payload, null, 2) }</code></pre>
+    <DialogContent sx={{ display: 'flex', flexDirection: 'column', overflow: 'hidden'}}>
+      <SyntaxHighlighter language="json" useInlineStyles={false} showLineNumbers PreTag={Pre}>
+        { formatted }
+      </SyntaxHighlighter>
     </DialogContent>
   </Dialog>;
 }
+
+// Utils
+const Pre = styled('pre')(({ theme }) => ({
+  flex: '1 1 auto',
+  overflow: 'auto',
+  margin: 0,
+  paddingLeft: theme.spacing(1),
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: theme.palette.grey['50'],
+  ...theme.applyStyles('dark', {
+    backgroundColor: theme.palette.background.paper,
+  })
+}));
