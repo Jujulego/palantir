c30ad94efa9ae6897d2dfe083d99e31b938fd949
Julien Capellari <julien.capellari@gmail.com>
2024-11-01 13:56:16 +0100

    feat: type dns response

--- data/dns.ts
+++ data/dns.ts
@@ -13,7 +13,36 @@ export async function reverseDnsLookup(ip: string) {
   }
 
   const res = await fetch(url);
-  const { Answer } = await res.json();
+  const response = (await res.json()) as DnsResponse;
 
-  return Answer?.[0]?.data;
+  if (response.Answer) {
+    return response.Answer[0].data.slice(0, -1);
+  } else {
+    return null;
+  }
+}
+
+export interface DnsResponse {
+  readonly Status: number;
+  readonly TC: boolean;
+  readonly RD: boolean;
+  readonly RA: boolean;
+  readonly AD: boolean;
+  readonly CD: boolean;
+  readonly Question: readonly DnsQuestion[],
+  readonly Answer?: readonly DnsAnswer[];
+  readonly Comment?: string;
+  readonly edns_client_subnet?: string;
+}
+
+export interface DnsQuestion {
+  readonly name: string;
+  readonly type: number;
+}
+
+export interface DnsAnswer {
+  readonly name: string;
+  readonly type: number;
+  readonly TTL: number;
+  readonly data: string;
 }
