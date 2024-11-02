2e9bc2d7b659fdc992d295b60f87c40c4bb611ec
Julien Capellari <julien.capellari@gmail.com>
2024-11-03 00:44:33 +0100

    fix: prevent middleware on monitoring route

--- middleware.ts
+++ middleware.ts
@@ -9,11 +9,12 @@ export const config = {
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
      * - _next/image (image optimization files)
+     * - monitoring (sentry tunnel)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    '/((?!_next/static|_next/image|monitoring|favicon.ico|sitemap.xml|robots.txt).*)',
   ]
-}
+};
 
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
   return router.run(request, event);
@@ -32,6 +33,6 @@ router
       return next();
     }
   })
-  .all(async (request, event) => {
+  .all(async (request) => {
     return await auth0.middleware(request);
   });
