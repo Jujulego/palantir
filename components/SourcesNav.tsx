23ef0315a988ff5e4868669eb51cde6a97f7675e
Julien Capellari <julien.capellari@gmail.com>
2024-11-04 12:10:17 +0100

    feat: add icon to all sources

--- components/SourcesNav.tsx
+++ components/SourcesNav.tsx
@@ -3,8 +3,11 @@
 import IpDataIcon from '@/components/icons/IpDataIcon';
 import IpInfoIcon from '@/components/icons/IpInfoIcon';
 import IpQualityScoreIcon from '@/components/icons/IpQualityScoreIcon';
+import CloudIcon from '@mui/icons-material/Cloud';
+import PlaceIcon from '@mui/icons-material/Place';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
+import type { SxProps, Theme } from '@mui/material/styles';
 import Link from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
 import type { ReactElement, ReactNode } from 'react';
@@ -12,11 +15,39 @@ import type { ReactElement, ReactNode } from 'react';
 // Component
 export default function SourcesNav() {
   return <Box component="nav" sx={{ display: 'flex', flexWrap: 'wrap', px: 2, pt: 1, gap: 1 }}>
-    <LinkChip sourceId="ip-info" label="IPinfo" icon={<IpInfoIcon />}  />
-    <LinkChip sourceId="ip-quality-score" label={<><span style={{ color: '#F43A3A' }}>IP</span>QS</>} icon={<IpQualityScoreIcon />} />
-    <LinkChip sourceId="ip-geolocation" label="ipgeolocation"  />
-    <LinkChip sourceId="ip-data" label="ipdata" icon={<IpDataIcon />}  />
-    <LinkChip sourceId="big-data-cloud" label="BigDataCloud" />
+    <LinkChip
+      sourceId="ip-info"
+      label="IPinfo"
+      icon={<IpInfoIcon />}
+    />
+    <LinkChip
+      sourceId="ip-quality-score"
+      label={<><em>IP</em>QS</>}
+      icon={<IpQualityScoreIcon />}
+      sx={{
+        em: {
+          color: '#F43A3A',
+          fontStyle: 'normal',
+        }
+      }}
+    />
+    <LinkChip
+      sourceId="ip-geolocation"
+      label="ipgeolocation"
+      icon={<PlaceIcon />}
+      sx={{ '.MuiChip-icon': { color: '#6C63FD' } }}
+    />
+    <LinkChip
+      sourceId="big-data-cloud"
+      label="BigDataCloud"
+      icon={<CloudIcon />}
+      sx={{ '.MuiChip-icon': { color: '#E36327' } }}
+    />
+    <LinkChip
+      sourceId="ip-data"
+      label="ipdata"
+      icon={<IpDataIcon />}
+    />
   </Box>;
 }
 
@@ -25,9 +56,10 @@ interface LinkChipProps {
   readonly sourceId: string;
   readonly label: ReactNode;
   readonly icon?: ReactElement;
+  readonly sx?: SxProps<Theme>;
 }
 
-function LinkChip({ sourceId, label, icon }: LinkChipProps) {
+function LinkChip({ sourceId, label, icon, sx }: LinkChipProps) {
   const selected = useSelectedLayoutSegment();
 
   return <Chip
@@ -35,5 +67,6 @@ function LinkChip({ sourceId, label, icon }: LinkChipProps) {
     label={label} icon={icon}
     clickable size="small"
     variant={selected === sourceId ? 'filled' : 'outlined'}
+    sx={sx}
   />;
 }
