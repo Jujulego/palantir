b5bf239fa2a09b6efbce098a22c71f73665f2498
Julien Capellari <julien.capellari@gmail.com>
2024-12-03 00:26:11 +0100

    Expand coverage (#184)
    * feat: test jsonFetch
    
    * feat: test useDnsLookup

---
+++ utils/fetch.test.ts
@@ -0,0 +1,37 @@
+import { FetchError, jsonFetch } from '@/utils/fetch';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+
+beforeEach(() => {
+  vi.spyOn(global, 'fetch');
+});
+
+afterEach(() => {
+  vi.resetAllMocks();
+});
+
+describe('jsonFetch', () => {
+  it('should call fetch and return parsed json output', async () => {
+    vi.mocked(fetch).mockResolvedValue({
+      ok: true,
+      json: async () => ({ test: 'successful' })
+    } as Response);
+
+    await expect(jsonFetch('https://www.example.com'))
+      .resolves.toStrictEqual({
+        test: 'successful',
+      });
+
+    expect(fetch).toHaveBeenCalledWith('https://www.example.com', undefined);
+  });
+
+  it('should throw a FetchError if api call is not ok', async () => {
+    vi.mocked(fetch).mockResolvedValue({
+      ok: false,
+      status: 418,
+      text: async () => 'test failed!'
+    } as Response);
+
+    await expect(jsonFetch('https://www.example.com'))
+      .rejects.toStrictEqual(new FetchError(418, 'test failed!'));
+  });
+});
--- utils/fetch.ts
+++ utils/fetch.ts
@@ -8,8 +8,8 @@ export class FetchError extends Error {
   }
 }
 
-export async function jsonFetch<D>(url: string | URL): Promise<D> {
-  const res = await fetch(url);
+export async function jsonFetch<D>(url: string | URL, options?: RequestInit): Promise<D> {
+  const res = await fetch(url, options);
 
   if (!res.ok) {
     throw new FetchError(res.status, await res.text());
