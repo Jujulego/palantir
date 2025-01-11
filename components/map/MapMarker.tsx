205c5dc2e8e5ebf8a525104a84afef7433e0eb0e
Julien Capellari <julien.capellari@gmail.com>
2025-01-11 13:19:45 +0100

    feat: setup custom marker

--- components/map/MapMarker.tsx
+++ components/map/MapMarker.tsx
@@ -2,29 +2,51 @@
 
 import { MapContext } from '@/components/map/map.context';
 import { useLazyMapbox } from '@/hooks/useLazyMapbox';
-import { use, useCallback } from 'react';
+import PlaceIcon from '@mui/icons-material/Place';
+import type * as mapboxgl from 'mapbox-gl';
+import { use, useCallback, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 // Component
 export interface MapMarkerProps {
   readonly latitude: number;
   readonly longitude: number;
-  readonly color?: string;
 }
 
-export default function MapMarker({ color, latitude, longitude }: MapMarkerProps) {
+export default function MapMarker({ latitude, longitude }: MapMarkerProps) {
+  const elementRef = useRef(document.createElement('div'));
+  const markerRef = useRef<mapboxgl.Marker>(null);
   const { map, isLoaded } = use(MapContext);
-
+  
   useLazyMapbox(useCallback(({ Marker }) => {
-    if (!map || !isLoaded) return;
+    markerRef.current = new Marker({ element: elementRef.current })
+      .setLngLat({ lat: latitude, lng: longitude });
 
-    const marker = new Marker({ color })
-      .setLngLat({ lat: latitude, lng: longitude })
-      .addTo(map);
+    if (map && isLoaded) {
+      markerRef.current.addTo(map);
+    }
+  }, [])); /* eslint-disable-line react-hooks/exhaustive-deps */
+
+  useEffect(() => {
+    const marker = markerRef.current;
+    if (!map || !isLoaded || !marker) return;
+
+    marker.addTo(map);
 
     return () => {
       marker.remove();
     };
-  }, [map, isLoaded, color, latitude, longitude]));
+  }, [isLoaded, map]);
 
-  return null;
+  useEffect(() => {
+    if (!markerRef.current) return;
+    
+    markerRef.current.setLngLat({ lat: latitude, lng: longitude });
+  }, [latitude, longitude]);
+
+  // Render
+  return createPortal(
+    <PlaceIcon sx={{ position: 'absolute', bottom: -3, left: -18, color: 'primary.main', fontSize: 36 }} />,
+    elementRef.current,
+  );
 }
