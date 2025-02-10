38d82486eab966c4a5d0828966ac5ef299fcda4f
Julien Capellari <julien.capellari@gmail.com>
2025-02-10 19:16:24 +0100

    feat: add animal loading states (#318)

---
+++ app/(with-map)/animal/[name]/loading.tsx
@@ -0,0 +1,56 @@
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import PetsIcon from '@mui/icons-material/Pets';
+import StraightenIcon from '@mui/icons-material/Straighten';
+import WcIcon from '@mui/icons-material/Wc';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Skeleton from '@mui/material/Skeleton';
+
+export default function WithMapAnimalNameLoading() {
+  return (
+    <List>
+      <ListItem sx={{ minHeight: 56, px: 2, py: 0 }}>
+        <ListItemIcon sx={{ minWidth: 40 }}>
+          <LocationCityIcon color="primary" />
+        </ListItemIcon>
+
+        <ListItemText
+          primary={<Skeleton sx={{ width: '75%' }} />}
+          secondary={<Skeleton sx={{ width: '50%' }} />}
+        />
+      </ListItem>
+
+      <ListItem sx={{ minHeight: 56, px: 2, py: 0 }}>
+        <ListItemIcon sx={{ minWidth: 40 }}>
+          <PetsIcon color="primary" />
+        </ListItemIcon>
+
+        <ListItemText
+          primary={<Skeleton sx={{ width: '75%' }} />}
+        />
+      </ListItem>
+
+      <ListItem sx={{ minHeight: 56, px: 2, py: 0 }}>
+        <ListItemIcon sx={{ minWidth: 40 }}>
+          <StraightenIcon color="primary" />
+        </ListItemIcon>
+
+        <ListItemText
+          primary={<Skeleton sx={{ width: '75%' }} />}
+        />
+      </ListItem>
+
+      <ListItem sx={{ minHeight: 56, px: 2, py: 0 }}>
+        <ListItemIcon sx={{ minWidth: 40 }}>
+          <WcIcon color="primary" />
+        </ListItemIcon>
+
+        <ListItemText
+          primary={<Skeleton sx={{ width: '75%' }} />}
+        />
+      </ListItem>
+    </List>
+  );
+}
