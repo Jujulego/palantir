b6da5644ed02e99c3c035dff712a97aaa9dca4a3
Julien Capellari <julien.capellari@gmail.com>
2025-02-26 00:46:44 +0100

    feat: rework button style

--- components/users/UserRow.tsx
+++ components/users/UserRow.tsx
@@ -5,30 +5,37 @@ import VirtualRow, { type VirtualRowProps } from '@/components/table/VirtualRow'
 import UserAvatar from '@/components/users/UserAvatar';
 import FormatDate from '@/components/utils/FormatDate';
 import type { UserDto } from '@/lib/users/users';
-import { mergeSx } from '@/lib/utils/mui';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import PersonIcon from '@mui/icons-material/Person';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import { useRouter } from 'next/navigation';
-import { useCallback } from 'react';
+import ButtonBase from '@mui/material/ButtonBase';
+import Link from 'next/link';
 
 // Component
 export interface VirtualUserRowProps extends VirtualRowProps {
   readonly user: UserDto;
 }
 
-export default function UserRow({ user, sx, ...rest }: VirtualUserRowProps) {
-  const router = useRouter();
-  
-  const handleClick = useCallback(() => {
-    router.push(`/console/auth/users/${encodeURIComponent(user.user_id)}`);
-  }, [router, user.user_id]);
-
+export default function UserRow({ user, ...rest }: VirtualUserRowProps) {
   return (
-    <VirtualRow {...rest} hover onClick={handleClick} sx={mergeSx({ cursor: 'pointer' }, sx)}>
-      <VirtualCell scope="row" sx={{ display: 'flex', alignItems: 'center', py: 0, gap: 1 }}>
-        <UserAvatar user={user} sx={{ height: 24, width: 24 }} />
-        <span>{user.nickname ?? user.name}</span>
+    <VirtualRow {...rest} hover>
+      <VirtualCell scope="row" padding="none">
+        <ButtonBase
+          component={Link}
+          href={`/console/auth/users/${encodeURIComponent(user.user_id)}`}
+          sx={{
+            height: '100%',
+            px: 2,
+
+            display: 'flex',
+            justifyContent: 'unset',
+            alignItems: 'center',
+            gap: 1,
+          }}
+        >
+          <UserAvatar user={user} sx={{ height: 24, width: 24 }} />
+          <span>{user.nickname ?? user.name}</span>
+        </ButtonBase>
       </VirtualCell>
 
       <VirtualCell sx={{ display: 'flex', alignItems: 'center', py: 0, gap: 1 }}>
