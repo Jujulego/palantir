a836efe181c2b7da7ce6c3c89eeca9875969b414
Julien Capellari <julien.capellari@gmail.com>
2025-02-26 00:02:48 +0100

    feat: cursor !

--- components/users/UserRow.tsx
+++ components/users/UserRow.tsx
@@ -4,6 +4,7 @@ import VirtualCell from '@/components/table/VirtualCell';
 import VirtualRow, { type VirtualRowProps } from '@/components/table/VirtualRow';
 import FormatDate from '@/components/utils/FormatDate';
 import type { UserDto } from '@/lib/users/users';
+import { mergeSx } from '@/lib/utils/mui';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import PersonIcon from '@mui/icons-material/Person';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
@@ -16,7 +17,7 @@ export interface VirtualUserRowProps extends VirtualRowProps {
   readonly user: UserDto;
 }
 
-export default function UserRow({ user, ...rest }: VirtualUserRowProps) {
+export default function UserRow({ user, sx, ...rest }: VirtualUserRowProps) {
   const router = useRouter();
   
   const handleClick = useCallback(() => {
@@ -24,7 +25,7 @@ export default function UserRow({ user, ...rest }: VirtualUserRowProps) {
   }, [router, user.user_id]);
 
   return (
-    <VirtualRow {...rest} hover onClick={handleClick}>
+    <VirtualRow {...rest} hover onClick={handleClick} sx={mergeSx({ cursor: 'pointer' }, sx)}>
       <VirtualCell scope="row" sx={{ display: 'flex', alignItems: 'center', py: 0, gap: 1 }}>
         <Avatar src={user.picture} alt={user.nickname ?? user.name} sx={{ height: 24, width: 24 }} />
         <span>{user.nickname ?? user.name}</span>
