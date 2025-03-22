3b37587ce54ec821c04bbb36d0bba38d3ab53201
Julien Capellari <julien.capellari@gmail.com>
2025-03-23 00:13:10 +0100

    fix: build error

--- components/animal/AnimalMarker.tsx
+++ components/animal/AnimalMarker.tsx
@@ -4,6 +4,7 @@ import MapMarker from '@/components/map/MapMarker';
 import { useMapFlyTo } from '@/lib/map/useMapFlyTo';
 import type { Coordinates } from '@/lib/utils/coordinates';
 import PlaceIcon from '@mui/icons-material/Place';
+import NoSsr from '@mui/material/NoSsr';
 import { m } from 'motion/react';
 import { useEffect } from 'react';
 
@@ -23,28 +24,30 @@ export default function AnimalMarker({ coordinates }: AnimalMarkerProps) {
 
   // Render
   return (
-    <MapMarker latitude={coordinates.latitude} longitude={coordinates.longitude}>
-      <m.div
-        initial={{ scale: 0, translateY: '8.33%' }}
-        animate={{
-          scale: 1.325,
-        }}
-        style={{
-          transformOrigin: 'bottom center',
-        }}
-        transition={{
-          duration: 0.5,
-        }}
-      >
-        <PlaceIcon
-          sx={{
-            color: 'primary.main',
-            filter: 'drop-shadow(0px 3px 3px rgba(0, 0, 0, 0.2))',
-            fontSize: 36,
-            verticalAlign: 'bottom'
+    <NoSsr>
+      <MapMarker latitude={coordinates.latitude} longitude={coordinates.longitude}>
+        <m.div
+          initial={{ scale: 0, translateY: '8.33%' }}
+          animate={{
+            scale: 1.325,
           }}
-        />
-      </m.div>
-    </MapMarker>
+          style={{
+            transformOrigin: 'bottom center',
+          }}
+          transition={{
+            duration: 0.5,
+          }}
+        >
+          <PlaceIcon
+            sx={{
+              color: 'primary.main',
+              filter: 'drop-shadow(0px 3px 3px rgba(0, 0, 0, 0.2))',
+              fontSize: 36,
+              verticalAlign: 'bottom'
+            }}
+          />
+        </m.div>
+      </MapMarker>
+    </NoSsr>
   );
 }
