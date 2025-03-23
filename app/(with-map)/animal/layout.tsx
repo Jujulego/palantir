219460c333b8433a5b5986bcf6fe78d762145830
Julien Capellari <julien.capellari@gmail.com>
2025-03-23 14:22:58 +0100

    feat: control drawer illustration

--- app/(with-map)/animal/layout.tsx
+++ app/(with-map)/animal/layout.tsx
@@ -1,5 +1,6 @@
 import seaTurtlePng from '@/assets/sea-turtle.png';
 import MapDrawer from '@/components/map/drawer/MapDrawer';
+import MapDrawerIllustration from '@/components/map/drawer/MapDrawerIllustration';
 import ColoredImage from '@/components/utils/ColoredImage';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -12,21 +13,23 @@ export interface WithMapAnimalLayoutProps {
 export default function withMapAnimalLayout({ children }: WithMapAnimalLayoutProps) {
   return (
     <MapDrawer>
-      <Box
-        sx={{
-          position: 'relative',
-          height: 230,
-          flexShrink: 0,
-          color: 'action.selected',
-        }}
-      >
-        <ColoredImage
-          src={seaTurtlePng}
-          alt="sea turtle"
-          priority
-          style={{ position: 'absolute', bottom: 0, left: 'calc(50% - 75px)', height: 'auto', width: 150 }}
-        />
-      </Box>
+      <MapDrawerIllustration>
+        <Box
+          sx={{
+            position: 'relative',
+            height: 230,
+            flexShrink: 0,
+            color: 'action.selected',
+          }}
+        >
+          <ColoredImage
+            src={seaTurtlePng}
+            alt="sea turtle"
+            priority
+            style={{ position: 'absolute', bottom: 0, left: 'calc(50% - 75px)', height: 'auto', width: 150 }}
+          />
+        </Box>
+      </MapDrawerIllustration>
 
       <Paper square sx={{ flex: '1 0 auto' }}>
         { children }
--- app/(with-map)/server/layout.tsx
+++ app/(with-map)/server/layout.tsx
@@ -1,4 +1,5 @@
 import computerPng from '@/assets/computer.png';
+import MapDrawerIllustration from '@/components/map/drawer/MapDrawerIllustration';
 import ColoredImage from '@/components/utils/ColoredImage';
 import MapDrawer from '@/components/map/drawer/MapDrawer';
 import Box from '@mui/material/Box';
@@ -12,21 +13,23 @@ export interface WithMapServerLayoutProps {
 export default function WithMapServerLayout({ children }: WithMapServerLayoutProps) {
   return (
     <MapDrawer>
-      <Box
-        sx={{
-          position: 'relative',
-          height: 230,
-          flexShrink: 0,
-          color: 'action.selected',
-        }}
-      >
-        <ColoredImage
-          src={computerPng}
-          alt="computer"
-          priority
-          style={{ position: 'absolute', bottom: 0, left: 'calc(50% - 75px)', height: 'auto', width: 150 }}
-        />
-      </Box>
+      <MapDrawerIllustration>
+        <Box
+          sx={{
+            position: 'relative',
+            height: 230,
+            flexShrink: 0,
+            color: 'action.selected',
+          }}
+        >
+          <ColoredImage
+            src={computerPng}
+            alt="computer"
+            priority
+            style={{ position: 'absolute', bottom: 0, left: 'calc(50% - 75px)', height: 'auto', width: 150 }}
+          />
+        </Box>
+      </MapDrawerIllustration>
 
       <Paper square sx={{ flex: '1 0 auto' }}>
         { children }
