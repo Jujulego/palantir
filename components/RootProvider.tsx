580d7e97fa7cebfe859a9611810d52edc0cbc4c6
Julien Capellari <julien.capellari@gmail.com>
2025-07-03 23:19:00 +0200

    fix: build errors

--- components/RootProvider.tsx
+++ components/RootProvider.tsx
@@ -4,19 +4,22 @@ import theme from '@/theme';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { domAnimation, LazyMotion } from 'motion/react';
 import type { ReactNode } from 'react';
 
-export interface MuiProviderProps {
+export interface RootProviderProps {
   readonly children: ReactNode;
 }
 
-export default function MuiProvider({ children }: MuiProviderProps) {
+export default function RootProvider({ children }: RootProviderProps) {
   return (
     <AppRouterCacheProvider options={{ enableCssLayer: true }}>
       <ThemeProvider theme={theme}>
         <CssBaseline enableColorScheme />
 
-        { children }
+        <LazyMotion features={domAnimation} strict>
+          { children }
+        </LazyMotion>
       </ThemeProvider>
     </AppRouterCacheProvider>
   );
