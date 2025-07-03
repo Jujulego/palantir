40a145d76cda220b06cc5489878537a51c3b4824
Julien Capellari <julien.capellari@gmail.com>
2025-07-03 09:01:31 +0200

    feat: simplify layer stack

--- theme.ts
+++ theme.ts
@@ -61,7 +61,7 @@ const theme = createTheme({
   cssVariables: {
     colorSchemeSelector: 'class'
   },
-  modularCssLayers: '@layer theme, base, mapbox, mui, components, utilities;',
+  modularCssLayers: '@layer mapbox, mui;',
   palette: {
     mode: 'light',
     bigDataCloud: base.palette.augmentColor({
