b9a3e604d28a3b2ab4b2e47daa5235cf73252a73
Julien Capellari <julien.capellari@gmail.com>
2025-08-05 10:42:49 +0200

    fix: validate animal name in action

--- lib/animal/club-ocean.actions.ts
+++ lib/animal/club-ocean.actions.ts
@@ -1,7 +1,12 @@
 'use server';
 
 import { scrapAnimalTracking } from '@/lib/animal/club-ocean';
+import { ANIMAL_NAME_REGEX } from '@/lib/animal/constants';
 
 export async function actScrapAnimalTracking(name: string) {
+  if (!ANIMAL_NAME_REGEX.test(name)) {
+    return null;
+  }
+
   return await scrapAnimalTracking(name);
 }
